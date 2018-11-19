22e929a16a8a0ffd44a458da26c179f5a3a79576
Jason Rice <ricejasonf@gmail.com>
2018-11-19 14:01:42 -0800

    Add lambda capture double whammy expansion example

---
+++ play/test10.cpp
@@ -0,0 +1,21 @@
+#include <cassert>
+#include <utility>
+
+template <typename T>
+struct foo;
+
+template <unsigned long ...i>
+struct foo<std::index_sequence<i...>> {
+  using apply(auto ...x) {
+    return [&] {
+      static_assert(sizeof...(i) == 3);
+      static_assert(sizeof...(x) == 3);
+      return ((x + i) + ...);
+    }();
+  }
+};
+
+int main() {
+  int x = foo<std::make_index_sequence<3>>::apply(25 , 15, 2);
+  assert(x == 45);
+}
