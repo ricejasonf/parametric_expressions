fb2c2c8284328a20db7a44ae9ba856d88dd1c949
Jason Rice <ricejasonf@gmail.com>
2018-11-30 10:46:18 -0800

    test for operator names (unsupported)

---
+++ play/test13.cpp
@@ -0,0 +1,15 @@
+#include <cassert>
+
+struct foo {
+  int value;
+
+  using operator||(using auto x) {
+    if (value) return *this;
+    else return foo{x};
+  }
+};
+
+int main() {
+  foo result = foo{5} || foo{6};
+  //assert(result.value == 5);
+}
