6592715f119c9bc61ae25f2197ecc6dfdeb889b9
Jason Rice <ricejasonf@gmail.com>
2018-12-04 17:00:40 -0800

    Test instantiated members

--- play/test7.cpp
+++ play/test7.cpp
@@ -11,11 +11,14 @@ using addp(using auto ...x) {
 
 template <typename T>
 struct foo {
+  int value;
+
   using apply(using auto f, auto ...x) {
-    return f(x...);
+    return f(value, x...);
   }
 };
 
+// bar/baz are a simplified test of a bugfix
 using baz(using auto x) {
   return x;
 }
@@ -25,12 +28,12 @@ using bar(auto y) {
 }
 
 int main() {
-  int x = foo<int>::apply(add, 25, 15, 2);
-                       // ^ Overloaded Function!!
+  int x = foo<int>{1}.apply(add, 25, 15, 1);
+                         // ^ Overloaded Function!!
   assert(x == 42);
 
-  int y = foo<int>::apply(addp, 25, 15, 2);
-                       // ^ Parametric Expression Id!!
+  int y = foo<int>{1}.apply(addp, 25, 15, 1);
+                         // ^ Parametric Expression Id!!
   assert(y == 42);
 
   assert(bar(5) == 5);
--- play/test9.cpp
+++ play/test9.cpp
@@ -1,16 +1,11 @@
-#include <cassert>
-#include <utility>
-
-template <typename ...T>
-int add(T ...x) {
-  return (x + ...);
-}
+template <unsigned long ...>
+struct index_sequence;
 
 template <typename T>
 struct foo;
 
 template <unsigned long ...i>
-struct foo<std::index_sequence<i...>> {
+struct foo<index_sequence<i...>> {
   using apply(auto ...x) {
     static_assert(sizeof...(i) == 5);
     static_assert(sizeof...(x) == 3);
@@ -20,6 +15,5 @@ struct foo<std::index_sequence<i...>> {
 };
 
 int main() {
-  int x = foo<std::make_index_sequence<5>>::apply(25 , 15, 2);
-  assert(x == 45);
+  int x = foo<index_sequence<0, 1, 2, 3, 4>>::apply(25 , 15, 2);
 }
