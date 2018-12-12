3a8ce04ee8709196667bf6d573984cd14600b62f
Jason Rice <ricejasonf@gmail.com>
2018-12-12 10:47:08 -0800

    Finish test for binary op lookup

--- play/test13.cpp
+++ play/test13.cpp
@@ -1,15 +1,51 @@
-//#include <cassert>
+#include <cassert>
 
-struct foo {
-  int value;
+namespace blah {
+  struct foo {
+    int value;
 
-  using operator||(using auto x) {
-    if (value) return *this;
-    else return foo{x};
+    // member candidate
+    using operator||(auto self, using auto x) {
+      if (self.value) return self;
+      else return foo{x};
+    }
+  };
+
+  // non-member candidate found via ADL
+  using operator==(using auto self, using auto x) {
+    return self.value == x.value;
   }
-};
+}
+
+// non-member candidate
+using operator&&(auto self, using auto x) {
+  if (self.value) return blah::foo{x};
+  else return self;
+}
 
 int main() {
-  foo result = foo{5} || foo{6};
-  //assert(result.value == 5);
+  using foo = blah::foo;
+
+  //member
+
+  {
+    foo result1 = foo{5} || foo{6};
+    assert(result1.value == 5);
+
+    foo result2 = foo{0} || foo{6};
+    assert(result2 == foo{6});
+
+    foo result3 = foo{5}.operator||(6);
+    assert(result3 == foo{5});
+  }
+
+  // non-member
+
+  {
+    foo result1 = foo{5} && foo{6};
+    assert(result1 == foo{6});
+
+    foo result2 = foo{0} && foo{6};
+    assert(result2 == foo{0});
+  }
 }
