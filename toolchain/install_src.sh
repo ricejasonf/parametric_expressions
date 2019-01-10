2d3315c18cea983b9f779bb6e27042841a712b6d
Jason Rice <ricejasonf@gmail.com>
2019-01-09 16:37:16 -0800

    Use script to install source files

---
+++ toolchain/install_src.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cppdock install_src e90d44039a60317e8379232d5c43dc06cef8f463 \
+                    llvm-mirror/llvm      ./llvm
+cppdock install_src 32d0706258dae2bff19b6879f9b03aa853992712 \
+                    ricejasonf/clang      ./llvm/tools/clang
+cppdock install_src 61b0a0088b54793b2d4e7fc2f1b1a1f3893293a8 \
+                    llvm-mirror/libcxx    ./llvm/projects/libcxx
+cppdock install_src bd80f6625dec55d43bab409ab0b71b94472eff28 \
+                    llvm-mirror/libcxxabi ./llvm/projects/libcxxabi
