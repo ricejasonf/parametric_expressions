accc62292255e6d3130e148527291f55d92642a5
Jason Rice <ricejasonf@gmail.com>
2019-01-14 17:53:33 -0800

    Update version of clang

--- toolchain/install_src.sh
+++ toolchain/install_src.sh
@@ -2,7 +2,7 @@
 
 cppdock install_src e90d44039a60317e8379232d5c43dc06cef8f463 \
                     llvm-mirror/llvm      ./llvm
-cppdock install_src 32d0706258dae2bff19b6879f9b03aa853992712 \
+cppdock install_src 83e8cc7bf47b20fd94802a0934587f3cb9270306 \
                     ricejasonf/clang      ./llvm/tools/clang
 cppdock install_src 61b0a0088b54793b2d4e7fc2f1b1a1f3893293a8 \
                     llvm-mirror/libcxx    ./llvm/projects/libcxx
