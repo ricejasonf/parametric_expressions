9bab2c211f344e7fd5de5915bb55d04c8c8865ee
Jason Rice <ricejasonf@gmail.com>
2019-01-31 17:11:40 -0800

    Updates P1221 clang version

--- toolchain/install_src.sh
+++ toolchain/install_src.sh
@@ -2,7 +2,7 @@
 
 cppdock install_src e90d44039a60317e8379232d5c43dc06cef8f463 \
                     llvm-mirror/llvm      ./llvm
-cppdock install_src 478fa54351030accfd53d084bf9370cdb60c7130 \
+cppdock install_src 3ad4cfd206f7cc8b9a241302eef965f45b15ba36 \
                     ricejasonf/clang      ./llvm/tools/clang
 cppdock install_src 61b0a0088b54793b2d4e7fc2f1b1a1f3893293a8 \
                     llvm-mirror/libcxx    ./llvm/projects/libcxx
